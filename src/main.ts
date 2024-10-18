72b842db92af64caa8688ba9dbb38c2258355d35
michael-azogu <michaelazogu101@gmail.com>
2024-10-18 16:14:32 +0100

    accelerated panning

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,17 @@
 import './style.css'
+import './pixelon.otf'
 import type { box, position } from './types'
 import {
   w,
   h,
-  create_canvas,
-  draw_image,
-  point_in_box,
   size,
   throttle,
+  draw_image,
+  point_in_box,
+  create_canvas,
+  invader_image,
   defender_image,
   explosion_image,
-  invader_image,
 } from './utils'
 
 const { main, draw, stack, layer } = create_canvas(w, h + size)
@@ -25,6 +26,7 @@ let score = 0
 const hiscore_key = 'hiscore'
 let hiscore = +(localStorage.getItem(hiscore_key) || 0)
 
+let explosions: Array<position> = []
 let invader_bullets: Array<position> = []
 let defender_bullets: Array<position> = []
 
@@ -43,8 +45,6 @@ for (let n = 0, x = 0; n < nbarriers; x += barrier_width, n++) {
   })
 }
 
-let explosions: Array<position> = []
-
 const defender: Pick<position, 'x'> & { readonly y: number } & box = {
   x: w / 2 - size / 2,
   y: h - size,
@@ -52,15 +52,35 @@ const defender: Pick<position, 'x'> & { readonly y: number } & box = {
   h: size / 2,
 }
 
-document.addEventListener('keydown', (e) => {
-  defender.x += e.code == 'ArrowLeft' ? -3 : e.key == 'ArrowRight' ? 3 : 0
-  defender.x =
-    defender.x < 0
-      ? 0
-      : defender.x > w - defender.w
-      ? w - defender.w
-      : defender.x
-})
+let last_pan_time = 0
+const avg_press_duration = 120 // based on avg defaults & xset q | grep "repeat delay"
+const base_pan_velocity = w / (4.5 * 1000)
+let pan_velocity = base_pan_velocity
+const pan_acceleration = (1.03 * base_pan_velocity) / (avg_press_duration * 2)
+
+function pan(e: KeyboardEvent) {
+  if (['ArrowLeft', 'ArrowRight'].includes(e.code)) {
+    let dt = avg_press_duration
+    if (e.repeat) {
+      const now = Date.now()
+      dt = last_pan_time != 0 ? now - last_pan_time : dt
+      last_pan_time = now
+      pan_velocity += pan_acceleration * dt
+    } else {
+      last_pan_time = 0
+      pan_velocity = base_pan_velocity
+    }
+    const dx = pan_velocity * dt
+    defender.x += e.code == 'ArrowLeft' ? -dx : e.key == 'ArrowRight' ? dx : 0
+    defender.x =
+      defender.x < 0
+        ? 0
+        : defender.x > w - defender.w
+        ? w - defender.w
+        : defender.x
+  }
+}
+document.addEventListener('keydown', pan)
 
 function shoot(e: KeyboardEvent) {
   if (e.code == 'Space') {
@@ -70,15 +90,15 @@ function shoot(e: KeyboardEvent) {
     })
   }
 }
-
 document.addEventListener('keydown', throttle(shoot, 250))
 
 let invaders: Array<position & box> = []
+const per_row = 12
 const invader_gap = size * 0.4
 for (let r = 0, y = 0; r < 3; r++, y += invader_gap + size) {
   for (
-    let c = 0, x = w / 2 - (8 * size + invader_gap * 7) / 2;
-    c < 8;
+    let c = 0, x = w / 2 - (per_row * size + invader_gap * (per_row - 1)) / 2;
+    c < per_row;
     c++, x += invader_gap + size
   ) {
     invaders.push({
@@ -92,19 +112,20 @@ for (let r = 0, y = 0; r < 3; r++, y += invader_gap + size) {
 
 let game_over = false
 
-let previous_time = Date.now()
 let last_shot = Date.now()
+let previous_time = Date.now()
 
 let bullet_speed = h / (10 * 1000)
 let invader_speed = h / (75 * 1000)
 
-function game() {
+function game_loop() {
   const now = Date.now()
   const dt = now - previous_time
-  previous_time = Date.now()
+  previous_time = now
 
   const bullet_dy = dt * bullet_speed
 
+  // TODO bullet comes out at full speed, gradually slowing down just before exiting screen
   for (const bullet of invader_bullets) bullet.y += bullet_dy
   for (const bullet of defender_bullets) bullet.y -= bullet_dy
 
@@ -113,10 +134,9 @@ function game() {
     for (const barrier of barriers) {
       if (barrier.hits_left > 0) {
         for (const bullet of invader_bullets) {
-          if (point_in_box(bullet, barrier)) {
-            barrier.hits_left--
-            blocked_by_barrier.push(bullet)
-          }
+          if (!point_in_box(bullet, barrier)) continue
+          barrier.hits_left--
+          blocked_by_barrier.push(bullet)
         }
       }
     }
@@ -129,20 +149,20 @@ function game() {
 
     for (const invader of invaders) {
       for (const bullet of defender_bullets) {
-        if (point_in_box(bullet, invader)) {
-          hiscore = Math.max((score += 10), hiscore)
-          localStorage.setItem(hiscore_key, String(hiscore))
+        if (!point_in_box(bullet, invader)) continue
 
-          shots_landed.push(bullet)
-          destroyed_invaders.push(invader)
+        hiscore = Math.max((score += 10), hiscore)
+        localStorage.setItem(hiscore_key, String(hiscore))
 
-          const explosion = { y: invader.y, x: invader.x }
-          setTimeout(
-            () => (explosions = explosions.filter((e) => e != explosion)),
-            2500
-          )
-          explosions.push(explosion)
-        }
+        shots_landed.push(bullet)
+        destroyed_invaders.push(invader)
+
+        const explosion = { y: invader.y, x: invader.x }
+        setTimeout(
+          () => (explosions = explosions.filter((e) => e != explosion)),
+          2500
+        )
+        explosions.push(explosion)
       }
     }
     invaders = invaders.filter(
@@ -154,10 +174,9 @@ function game() {
 
     const blows_taken: position[] = []
     for (const bullet of invader_bullets) {
-      if (point_in_box(bullet, defender)) {
-        game_over = --lives == 0
-        blows_taken.push(bullet)
-      }
+      if (!point_in_box(bullet, defender)) continue
+      game_over = --lives == 0
+      blows_taken.push(bullet)
     }
     invader_bullets = invader_bullets.filter(
       (bullet) => !blows_taken.includes(bullet)
@@ -173,98 +192,82 @@ function game() {
   for (const invader of invaders) {
     invader.y += invader_dy
     game_over = invader.y > h
-    if (Math.random() > 0.5 && Date.now() - last_shot > 2000) {
+    if (
+      Math.random() > 0.5 &&
+      Date.now() - last_shot > Math.random() * 3000 + 2000
+    ) {
       if (Math.random() > 0.5) continue
-
       last_shot = Date.now()
       invader_bullets.push({
         x: invader.x + size / 2,
-        y: invader.y + size,
+        y: invader.y + size / 2,
       })
     }
   }
 
   draw(bullets_layer, (ctx) => {
-    bullets: {
-      for (const bullet of invader_bullets) {
-        ctx.beginPath()
-        ctx.fillStyle = 'red'
-        ctx.arc(bullet.x, bullet.y, size / 5, 0, Math.PI)
-        ctx.fill()
-        ctx.closePath()
-      }
-
-      for (const bullet of defender_bullets) {
-        ctx.beginPath()
-        ctx.fillStyle = 'blue'
-        ctx.arc(bullet.x, bullet.y, size / 5, 0, Math.PI, true)
-        ctx.fill()
-        ctx.closePath()
-      }
+    for (const bullet of invader_bullets) {
+      ctx.beginPath()
+      ctx.fillStyle = 'red'
+      ctx.arc(bullet.x, bullet.y, size / 5, 0, Math.PI)
+      ctx.fill()
+      ctx.closePath()
+    }
+    for (const bullet of defender_bullets) {
+      ctx.beginPath()
+      ctx.fillStyle = 'blue'
+      ctx.arc(bullet.x, bullet.y, size / 5, 0, Math.PI, true)
+      ctx.fill()
+      ctx.closePath()
     }
   })
 
   draw(mega_layer, (ctx) => {
-    barriers: {
-      ctx.fillStyle = 'brown'
-      for (const { x, y, w, h, hits_left } of barriers) {
-        if (hits_left > 0) {
-          ctx.font = `bold  ${h * 1.25}px pixelon`
-          ctx.fillText(
-            String(hits_left).padStart(2, '0'),
-            x + w / 2 - h,
-            y - h / 2
-          )
-          ctx.rect(x, y, w, h)
-        }
-      }
-      ctx.fill()
+    ctx.fillStyle = 'brown'
+    for (const { x, y, w, h, hits_left } of barriers) {
+      if (hits_left <= 0) continue
+      ctx.rect(x, y, w, h)
+      ctx.font = `bold  ${h * 1.25}px pixelon`
+      ctx.fillText(String(hits_left).padStart(2, '0'), x + w / 2 - h, y - h / 2)
+    }
+    ctx.fill()
+
+    for (const explosion of explosions) {
+      draw_image(ctx, explosion_image).at(explosion)
     }
 
-    defender: {
-      draw_image(ctx, defender_image).at({
-        y: defender.y,
-        x: defender.x,
+    for (const invader of invaders) {
+      draw_image(ctx, invader_image).at({
+        y: invader.y,
+        x: invader.x,
       })
     }
 
-    explosions: {
-      for (const explosion of explosions) {
-        draw_image(ctx, explosion_image).at(explosion)
-      }
-    }
+    draw_image(ctx, defender_image).at({
+      y: defender.y,
+      x: defender.x,
+    })
 
-    invaders: {
-      for (const invader of invaders) {
-        draw_image(ctx, invader_image).at({
-          y: invader.y,
-          x: invader.x,
-        })
-      }
-    }
+    ctx.font = '24px pixelon'
+    const stats = `           ${Array(lives)
+      .fill('❤️')
+      .join(' ')
+      .padEnd(3, '💥')}  SCORE: ${String(score).padStart(
+      5,
+      '0'
+    )}  RECORD: ${String(hiscore).padStart(5, '0')}`
 
-    stats: {
-      ctx.font = '24px pixelon'
-      const stats = `           ${Array(lives)
-        .fill('❤️')
-        .join(' ')
-        .padEnd(3, ' 💥')}  SCORE: ${String(score).padStart(
-        5,
-        '0'
-      )}  RECORD: ${String(hiscore).padStart(5, '0')}`
-
-      ctx.fillStyle = '#ccc'
-      ctx.fillRect(0, main.height - size, w, size)
-      ctx.strokeRect(0, main.height - size, w, size)
-      ctx.fillStyle = '#444'
-      ctx.fillText(String(stats), 0, main.height - 3)
-      ctx.strokeText(String(stats), 0, main.height - 3)
-    }
+    ctx.fillStyle = '#ccc'
+    ctx.fillRect(0, main.height - size, w, size)
+    ctx.strokeRect(0, main.height - size, w, size)
+    ctx.fillStyle = '#444'
+    ctx.fillText(String(stats), 0, main.height - 3)
+    ctx.strokeText(String(stats), 0, main.height - 3)
   })
 
-  return !game_over ? requestAnimationFrame(game) : location.reload()
+  return !game_over ? requestAnimationFrame(game_loop) : location.reload()
 }
 
-requestAnimationFrame(game)
+requestAnimationFrame(game_loop)
 
 document.getElementById('app')!.append(main)
